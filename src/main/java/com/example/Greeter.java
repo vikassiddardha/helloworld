279e7744f6c2e9d0da81f182084d0d7a92bbdb70
Siddardha Vikas <vikassiddardha@gmail.com>
2017-12-13 23:49:23 -0600

    testing

--- src/main/java/com/example/Greeter.java
+++ src/main/java/com/example/Greeter.java
@@ -13,7 +13,10 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
+  //public String greet(String someone) {
+    //return String.format("Hello, %s!", someone);
+  //}
+  public static void main(String[] args){
+ 	System.out.println("Hello");
   }
 }
